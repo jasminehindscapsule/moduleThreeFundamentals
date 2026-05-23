4a2e619dc54008f904ad1c514413bad4e909fc73
jasminehindscapsule <jasminehinds6@gmail.com>
2026-05-23 15:54:17 +1000

    Fix syntax error in getGreeting function definition

--- fundamentals5.js
+++ fundamentals5.js
@@ -1,11 +1,6 @@
 // a
-const name = getGreeting() {
-return 'Hello ' + name + '!';
-}
-
-console.log(getGreeting('Jasmine'));
-
 const getGreeting = (name) => {
 return 'Hello ' + name + '!';
 }
 
+console.log(getGreeting('Jasmine'));
